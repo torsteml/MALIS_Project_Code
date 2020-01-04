8d3614d3e1d82c4d97cd5a6c3f4703b804216789
Torstein Langan <langan@eurecom.fr>
2020-01-04 14:39:31 +0100

    Pushes

--- MovieScraper.py
+++ MovieScraper.py
@@ -1,52 +1,40 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[198]:
-
-
 import seaborn as sns
 import numpy as np
 import pandas as pd
+from time import sleep
+import requests
 
-
-# In[209]:
-
+url = "https://api.themoviedb.org/3/"
+api_key = "936803e93cf0f57238bc692f7cfed8d6"
 
 movies_dataframe = pd.read_csv('../dataset/movie_titles.txt', usecols=range(3), encoding='latin1', header=None) #Movie title may contain comma.
 movies_dataframe.columns = ['MovieID','YearOfRelease','Title']
-#movies_dataframe = movies_dataframe[:10]
-movies_dataframe
 
-
-# In[227]:
-
-
-import requests
 #returns the internal ID for use towards the Movie DB api. If no movie found, returns -1
 def getTheMovieDbId(title, year):
-    url = "https://api.themoviedb.org/3/search/movie"
-    api_key = "936803e93cf0f57238bc692f7cfed8d6"
-    api_key = "936803e93cf0f57238bc692f7cfed8d6"
     payload = {"api_key":api_key, "language":"en-US", "query":title, "include_adult":"false", "year":year}
-    r = requests.get(url, params=payload)
+    r = requests.get(url + 'search/movie', params=payload)
     data = r.json()
     choise = 0
-    try: 
+    try:
+        if(data['total_results']>1):
+            if(data['results'][0]['title'].startswith(title) and data['results'][0]['release_date'][:4] == str(year)):
+                choise = 0
+            else:
+                return -1
+    except:
+        pass
+    try:
         return data['results'][choise]['id']
     except:
         return -1
-    
+
+#Returns the information struct from the API
 def getMovieInformation(tmdbID):
-    url = "https://api.themoviedb.org/3/movie/" + str(tmdbID)
-    api_key = "936803e93cf0f57238bc692f7cfed8d6"
     payload = {"api_key":api_key, "language":"en-US"}
-    r = requests.get(url, params=payload)
+    r = requests.get(url + 'movie/' + str(tmdbID), params=payload)
     data = r.json()
     return data
-
-
-# In[204]:
-
 
 movies_dataframe['Budget'] = 0
 movies_dataframe['Genre'] = ''
@@ -68,74 +56,7 @@ for i in range(len(movies_dataframe)):
     else:
         print("Failed to append: ", title)
     print(i+1,"/",len(movies_dataframe)," (", (i+1)/len(movies_dataframe), ")")
-    
-movies_dataframe[:10]
+    sleep(2) #So we do not get banned from the API
 
+#Storing to CSV
 movies_dataframe.to_csv('../dataset/movies_with_features.csv', encoding='utf-8')
-
-
-# In[221]:
-
-
-movies_dataframe.loc[movies_dataframe['Title'] == 'The Road to Love']
-
-
-# In[222]:
-
-
-movies_dataframe.at[4014,'YearOfRelease']
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
